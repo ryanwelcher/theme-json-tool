ec7c9ec11095b67249b44942d5f5b1a37f40062d
Ryan Welcher <me@ryanwelcher.com>
2023-06-19 15:22:02 -0400

    initial commit

---
+++ json/settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+	border: {
+		color: true,
+	},
+	shadow: {
+		palette: [
+			{
+				name: 'Twitch2',
+				slug: 'twitch',
+				shadow: '5px 5px 0px -2px red, 5px 5px red',
+			},
+		],
+	},
+};
---
+++ theme-json.js
@@ -0,0 +1,65 @@
+const fs = require( 'fs' );
+const decache = require( 'decache' );
+const path = require( 'path' );
+//
+let base = require( './json/base.js' );
+let settings = require( './json/settings.js' );
+let styles = require( './json/styles.js' );
+let templateParts = require( './json/templateParts.js' );
+let customTemplates = require( './json/customTemplates.js' );
+
+const working_dir = path.resolve( process.cwd() );
+const theme_root = path.resolve( process.cwd(), '../' );
+const filename = 'theme.json';
+
+// Build Once
+buildThemeJSON( base, settings, styles, templateParts, customTemplates );
+
+// Watch for changes
+fs.watch( `${ working_dir }/json`, () => {
+	// Decache the files so we can get the latest version.
+	decache( './json/base.js' );
+	decache( './json/settings.js' );
+	decache( './json/styles.js' );
+	decache( './json/templateParts.js' );
+	decache( './json/customTemplates.js' );
+
+	// Re-require the files.
+	base = require( './json/base.js' );
+	settings = require( './json/settings.js' );
+	templateParts = require( './json/templateParts.js' );
+	templateParts = require( './json/templateParts.js' );
+	customTemplates = require( './json/customTemplates.js' );
+
+	// Rebuild the theme.json file.
+	buildThemeJSON( base, settings, styles, templateParts, customTemplates );
+} );
+
+/**
+ * Build the theme.json file.
+ */
+function buildThemeJSON(
+	base,
+	settings,
+	styles,
+	templateParts,
+	customTemplates
+) {
+	const themeJSON = {
+		...base,
+		settings,
+		styles,
+		templateParts,
+		customTemplates,
+	};
+	// Write the file.
+	fs.writeFile(
+		`${ theme_root }/${ filename }`,
+		JSON.stringify( themeJSON ),
+		( err ) => {
+			if ( ! err ) {
+				console.log( 'Theme.json rebuilt' );
+			}
+		}
+	);
+}

0f521f4ed6f55d033b443e27a8ca78b3621c113f
Ryan Welcher <me@ryanwelcher.com>
2023-06-21 14:04:42 -0400

    Pretty print the resulting JSON

--- theme-json.js
+++ theme-json.js
@@ -1,40 +1,40 @@
-const fs = require( 'fs' );
-const decache = require( 'decache' );
-const path = require( 'path' );
+const fs = require('fs');
+const decache = require('decache');
+const path = require('path');
 //
-let base = require( './json/base.js' );
-let settings = require( './json/settings.js' );
-let styles = require( './json/styles.js' );
-let templateParts = require( './json/templateParts.js' );
-let customTemplates = require( './json/customTemplates.js' );
+let base = require('./json/base.js');
+let settings = require('./json/settings.js');
+let styles = require('./json/styles.js');
+let templateParts = require('./json/templateParts.js');
+let customTemplates = require('./json/customTemplates.js');
 
-const working_dir = path.resolve( process.cwd() );
-const theme_root = path.resolve( process.cwd(), '../' );
+const workingDir = path.resolve(process.cwd());
+const themeRoot = path.resolve(process.cwd(), '../');
 const filename = 'theme.json';
 
 // Build Once
-buildThemeJSON( base, settings, styles, templateParts, customTemplates );
+buildThemeJSON(base, settings, styles, templateParts, customTemplates);
 
 // Watch for changes
-fs.watch( `${ working_dir }/json`, () => {
+fs.watch(`${workingDir}/json`, () => {
 	// Decache the files so we can get the latest version.
-	decache( './json/base.js' );
-	decache( './json/settings.js' );
-	decache( './json/styles.js' );
-	decache( './json/templateParts.js' );
-	decache( './json/customTemplates.js' );
+	decache('./json/base.js');
+	decache('./json/settings.js');
+	decache('./json/styles.js');
+	decache('./json/templateParts.js');
+	decache('./json/customTemplates.js');
 
 	// Re-require the files.
-	base = require( './json/base.js' );
-	settings = require( './json/settings.js' );
-	styles = require( './json/styles.js' );
-	templateParts = require( './json/templateParts.js' );
-	templateParts = require( './json/templateParts.js' );
-	customTemplates = require( './json/customTemplates.js' );
+	base = require('./json/base.js');
+	settings = require('./json/settings.js');
+	styles = require('./json/styles.js');
+	templateParts = require('./json/templateParts.js');
+	templateParts = require('./json/templateParts.js');
+	customTemplates = require('./json/customTemplates.js');
 
 	// Rebuild the theme.json file.
-	buildThemeJSON( base, settings, styles, templateParts, customTemplates );
-} );
+	buildThemeJSON(base, settings, styles, templateParts, customTemplates);
+});
 
 /**
  * Build the theme.json file.
@@ -55,11 +55,11 @@ function buildThemeJSON(
 	};
 	// Write the file.
 	fs.writeFile(
-		`${ theme_root }/${ filename }`,
-		JSON.stringify( themeJSON ),
-		( err ) => {
-			if ( ! err ) {
-				console.log( '🔥🔥🔥 theme.json updated. 🔥🔥🔥' );
+		`${themeRoot}/${filename}`,
+		JSON.stringify(themeJSON, null, '\t'),
+		(err) => {
+			if (!err) {
+				console.log('🔥🔥🔥 theme.json updated. 🔥🔥🔥');
 			}
 		}
 	);
